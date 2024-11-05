31fb6776c17a66d3d5f15b6b15b34329d2b895b0
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2024-11-05 17:28:15 +0100

    dashboard

---
+++ model/dj-query-catalog/energy.0.sql
@@ -0,0 +1,6 @@
+SELECT
+  "saref:Sensor"."id", "saref:Sensor"."status", floor, "datasheet"."minTemp", "datasheet"."maxTemp"
+FROM
+  "saref:Sensor", "assets", datasheet
+WHERE
+  id = assets."ID" AND datasheet."ID" = datasheet
